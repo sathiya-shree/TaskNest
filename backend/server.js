cae2ee365417d4dcefc39b00ab587ebf7694b9b2
sathiya-shree <159093269+sathiya-shree@users.noreply.github.com>
2025-05-27 11:06:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+// API routes
+const tasksRouter = require('./routes/tasks');
+app.use('/api/tasks', tasksRouter);
+
+// Root route to serve frontend index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
