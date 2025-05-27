f08602b097d112e537e03b20829a54bf18d043ad
sathiya-shree <159093269+sathiya-shree@users.noreply.github.com>
2025-05-27 09:20:05 +0530

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  text: String,
+  completed: Boolean,
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
 
